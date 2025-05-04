71f6e5e6e4a8e8622fcc8bf0680c666f50123703
gabszin <gsaraiva.contato@gmail.com>
2025-05-04 00:35:32 -0300

    Criação da página de admins, criação de exceções para erros de digitação, de validação do cpf, de valores primários já existentes e adição de uma função para validação de CPFs verdadeiros

--- core/users/api.py
+++ core/users/api.py
@@ -1,7 +1,22 @@
 from ninja import Router
+from .schemas import UserSchema
+from .models import User
+from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
 
 users_router = Router()
 
-@users_router.post('/', response={200 : dict})
-def create_user(request):
+@users_router.post('/', response={200 : dict, 400: dict, 500: dict})
+def create_user(request, user: UserSchema):
+    user = User(**user.dict())
+    user.password = make_password(user.password)
+    make_password(user.password)
+    try:
+        user.full_clean()
+        user.save()
+    except ValidationError as e:
+        return 400, {'errors': e.message_dict}
+    except Exception as e:
+        return 500, {'errors': 'Erro interno do servidor, contate um ADM'}
+    
     return{'ok': 'ok'}
